fd288a64f8e6e7c720532dd4c997fe2be0fd1280
Selimcan <webisdeveloper@gmail.com>
2024-09-11 14:53:08 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Loading } from "./components/Loading";
+
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const AppLayout = lazy(() => import("./layout/AppLayout"));
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="home" />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-orange-400 uppercase">📝 Todo List </h1>
+    </div>
+  );
+};
+
+export default Logo;
