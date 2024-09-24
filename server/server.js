a397a862f80c692bbc3e35d77406edc29f7d2062
selimcanozgur <webisdeveloper@gmail.com>
2024-09-24 18:57:19 +0300

    project updated

--- server/server.js
+++ server/server.js
@@ -2,10 +2,7 @@ import app from "./app.js";
 import mongoose from "mongoose";
 
 const port = process.env.PORT || 3000;
-const db_uri = process.env.DB_URI.replace(
-  "<db_password>",
-  process.env.DB_PASSWORD
-);
+const db_uri = process.env.DB_URI;
 
 mongoose
   .connect(db_uri)
